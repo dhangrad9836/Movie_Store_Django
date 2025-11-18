71f1463bf0d0d9a8dcd2cf6cdc89bc93435e45cf
Darren Dhanpat <darren.dhanpat@yahoo.com>
2025-11-18 12:57:04 -0500

    updated base template to render the web page title name, included a header and footer section

--- moviestore/home/views.py
+++ moviestore/home/views.py
@@ -5,12 +5,14 @@ from django.shortcuts import render
 # home view
 def index(request):
     template_data = {} # empty dictionary
-    template_data['title'] = 'Movie Store'  # create key called title and store it in the template_data dictionary
+    template_data['title'] = 'Movies Store'  # create key called title and store the 'Movie Store' title valuein the template_data dictionary
     return render(request, 'home/index.html', {
-        'template_data' : template_data     # third arg to pass in the template_data variable this will show when it's extended
-    })
+        'template_data' : template_data })    # third arg to pass in the template_data variable this will show when it's extended
+    
 
 
 # about view
 def about(request):
-    return render(request, 'home/about.html')
+    template_data = {} # empty dictionary
+    template_data['title'] = 'About'    # create a key called title and store the 'About' title value in the template_data dictonary
+    return render(request, 'home/about.html', { 'template_data' : template_data })
