6b5b773ef18ae359aac138a3a87464bdbfbe7d5a
Darren Dhanpat <darren.dhanpat@yahoo.com>
2025-11-18 10:36:45 -0500

    added home page, url path

---
+++ moviestore/home/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path    # path is used to define URL paterns
+from . import views # this is to import the views file
+
+# urlpatterns to define our list of paths
+urlpatterns = [
+    path('', views.index, name='home.index'),
+]
