b3b86a3246599d1d4d5e73c73ce410be0a53b855
Darren Dhanpat <darren.dhanpat@yahoo.com>
2025-11-18 12:12:11 -0500

    added comments for updated index function

--- moviestore/home/views.py
+++ moviestore/home/views.py
@@ -4,10 +4,10 @@ from django.shortcuts import render
 
 # home view
 def index(request):
-    template_data = {}
-    template_data['title'] = 'Movie Store'
+    template_data = {} # empty dictionary
+    template_data['title'] = 'Movie Store'  # create key called title and store it in the template_data dictionary
     return render(request, 'home/index.html', {
-        'template_data' : template_data
+        'template_data' : template_data     # third arg to pass in the template_data variable this will show when it's extended
     })
 
 
